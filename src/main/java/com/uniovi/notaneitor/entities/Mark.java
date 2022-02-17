0e41b63a94bf8fd534e91359be822836bd2d7248
uo264197 <71664251+uo264197@users.noreply.github.com>
2022-02-17 21:52:49 +0100

    SDI-2122-316-2.4-Modelo, acceso a datos simple.

--- src/main/java/com/uniovi/notaneitor/entities/Mark.java
+++ src/main/java/com/uniovi/notaneitor/entities/Mark.java
@@ -1,6 +1,11 @@
 package com.uniovi.notaneitor.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Mark {
+    @Id
+    @GeneratedValue
     private Long id;
     private String description;
     private Double score;
