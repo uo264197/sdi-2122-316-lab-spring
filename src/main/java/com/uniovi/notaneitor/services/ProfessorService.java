f18ce7db56fb4cf9c84695aee53dcae99e5ca2b7
uo264197 <uo264197@uniovi.es>
2022-02-18 10:55:01 +0100

    SDI-2022-316-sesión2-Ejercicio-complementario2

--- src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
+++ src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
@@ -1,6 +1,9 @@
 package com.uniovi.notaneitor.services;
 
 import com.uniovi.notaneitor.entities.Professor;
+import com.uniovi.notaneitor.repositories.MarksRepository;
+import com.uniovi.notaneitor.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +13,10 @@ import java.util.List;
 @Service
 public class ProfessorService {
 
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    /*
     private List<Professor> professorList = new LinkedList<>();
 
     @PostConstruct
@@ -17,20 +24,20 @@ public class ProfessorService {
         professorList.add(new Professor(1L, "1111111", "Profesor 1", "Apellidos 1", "Categoria 1"));
         professorList.add(new Professor(2L, "2222222", "Profesor 2","Apellidos 2",  "Categoria 2"));
     }
+     */
 
     public List<Professor> getProfessorList() {
+        List<Professor> professorList = new LinkedList<>();
+        professorRepository.findAll().forEach(professorList::add);
         return professorList;
     }
     public Professor getProfessor(Long id) {
-        return professorList.stream().filter(mark -> mark.getId().equals(id)).findFirst().get();
+        return professorRepository.findById(id).get();
     }
     public void addProfessor(Professor professor) {
-        if (professor.getId() == null) {
-            professor.setId(professorList.get(professorList.size() - 1).getId() + 1);
-        }
-        professorList.add(professor);
+        professorRepository.save(professor);
     }
     public void deleteProfessor(Long id) {
-        professorList.removeIf(professor -> professor.getId().equals(id));
+        professorRepository.deleteById(id);
     }
 }
