569f3ce5198cabf08db25cbea6111ed3eacfd222
uo264197 <71664251+uo264197@users.noreply.github.com>
2022-02-24 19:49:12 +0100

    SDI-2122-316-3.3-Acceso a datos con entidades relacionadas.

--- src/main/java/com/uniovi/notaneitor/entities/Mark.java
+++ src/main/java/com/uniovi/notaneitor/entities/Mark.java
@@ -10,6 +10,10 @@ public class Mark {
     private String description;
     private Double score;
 
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
     public Mark() {
     }
 
@@ -17,6 +21,13 @@ public class Mark {
         this.id = id;
         this.description = description;
         this.score = score;
+    }
+
+    public Mark(String description, Double score, User user) {
+        super();
+        this.description = description;
+        this.score = score;
+        this.user = user;
     }
 
     @Override
@@ -46,5 +57,13 @@ public class Mark {
 
     public void setScore(Double score) {
         this.score = score;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
