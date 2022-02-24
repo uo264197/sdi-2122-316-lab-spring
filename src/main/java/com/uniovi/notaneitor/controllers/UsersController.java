b5c2f17194688cd93388e9a50823a5a1a0c7d72b
uo264197 <71664251+uo264197@users.noreply.github.com>
2022-02-25 00:14:39 +0100

    SDI-2122-316-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -1,16 +1,22 @@
 package com.uniovi.notaneitor.controllers;
+import com.uniovi.notaneitor.SignUpFormValidator;
 import com.uniovi.notaneitor.services.SecurityService;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
 
 @Controller
 public class UsersController {
+
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @Autowired
     private UsersService usersService;
@@ -61,8 +67,19 @@ public class UsersController {
         return "redirect:/user/details/" + id;
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
