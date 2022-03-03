1756f4ecab45e64c3aee61badc7f9726daf4881f
uo264197 <71664251+uo264197@users.noreply.github.com>
2022-03-03 18:19:34 +0100

    SDI-2122-316-4.1-SpringBoot-Sesiones

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -12,8 +12,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class MarksController {
+
+    @Autowired
+    private HttpSession httpSession;
 
     @Autowired
     private MarkAddValidator markAddValidator;
@@ -29,6 +36,11 @@ public class MarksController {
 
     @RequestMapping("/mark/list")
     public String getList(Model model) {
+        Set<Mark> consultedList= (Set<Mark>) httpSession.getAttribute("consultedList");
+        if ( consultedList == null ) {
+            consultedList = new HashSet<Mark>();
+        }
+        model.addAttribute("consultedList", consultedList);
         model.addAttribute("markList", marksService.getMarks());
         return "mark/list";
     }
